a4d2c64d6694ea49c5f07179efaf337575a147c5
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2019-04-29 15:32:44 -0300

    Mostrando dados e imagem para o cliente na pagina profile

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,7 +33,7 @@ export class HomePage {
     this.auth.authenticate(this.creds)
     .subscribe(response => {
     this.auth.successfulLogin(response.headers.get('Authorization'));
-      this.navCtrl.setRoot('CategoriasPage');
+      this.navCtrl.setRoot('ProfilePage');
     }, 
     error=>{}) 
   }
